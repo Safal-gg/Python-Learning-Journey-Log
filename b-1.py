1afd220746c0d27f8bd161db23689fb94534b85e
Safal-gg <safal.shrestha355@gmail.com>
2025-09-13 16:41:39 +0545

    day1:started python learning

---
+++ b-1.py
@@ -0,0 +1,54 @@
+name="safal"
+age=19
+networth=150.89
+
+print("name")
+print(name)
+print(age)
+print(networth)
+
+
+name1='safal'
+name2="safal"
+name3='''safal'''
+
+print(name1 , name2 , name3)
+
+a=2
+b=3
+sum=a+b
+print(sum)
+
+#power operator
+var_1=6
+var_2=6
+print(var_1 ** var_2)
+
+#relational operator
+a=50
+b=20
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+print(a>b)
+print(a<b)
+
+#assignment operator
+num=10
+print("num:",num)
+num +=10
+print("num:",num)
+num -=10
+print("num:",num)
+num *=10
+print("num:",num)
+num /= 10
+print("num:",num)
+
+val1=True
+val2=True
+print("and operator", val1 and val2)
+print("or operator", val1 or val2)
+print("not operator", not val1)
+
