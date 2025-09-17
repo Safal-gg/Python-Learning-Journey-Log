9182c29209cacb337ceb47105c7b75236de9a968
Safal-gg <safal.shrestha355@gmail.com>
2025-09-17 22:49:43 +0545

    updated day-4 and added TIY-7 and TIY-8

--- d-4.py
+++ d-4.py
@@ -35,6 +35,44 @@ print(dimensions)
 for dimension in dimensions:
     print(dimension)
 
-#writing over tuple'
+#writing over tuple
 dimensions=(400,50)
 print(dimensions)
+
+#if-else statement
+cars=['bmw','audi','ford']
+
+for car in cars:
+    if car=='bmw':
+        print(car.upper())
+    
+    else:
+        print(car.title())
+
+
+#if-elif-else statement
+
+ages=[18,19,27,4,3,2]
+
+for age in ages:
+    if age<=4:
+        print("Your entrance fee is $0")
+    elif age<=18:
+        print("your entrance fee is $5")
+    else:
+        print("Your entrance fee is $10")
+
+
+#multiple if
+
+requested_toppings=['mushrooms','extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
+
+   
