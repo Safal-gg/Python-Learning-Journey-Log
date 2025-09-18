f3de85412234afa0a8de6b3e7a5a29419ef15ff1
Safal-gg <safal.shrestha355@gmail.com>
2025-09-19 00:15:32 +0545

    added day-5 and TIY-9

---
+++ TIY-9.py
@@ -0,0 +1,21 @@
+#Person:
+person={
+    'first_name':'sam',
+    'last_name':'altman',
+    'age':18,
+    'city':'san fransisco'
+}
+print(person)
+
+#Glossary:
+Glossary={
+    'list':'A order collection of items',
+    'slice':'A part of list',
+    'tuple':'An immutable list',
+    'dictionary':'A collection of key value pairs'
+}
+
+print(f"List:{Glossary['list'].title()}")
+print(f"Slice:{Glossary['slice'].title()}")
+print(f"Tuple:{Glossary['tuple'].title()}")
+print(f"Dictionary:{Glossary['dictionary'].title()}")
